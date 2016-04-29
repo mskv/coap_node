c1c71684f9a74abc9f923b20b59ba175c328d898
Michal Podworny <michal.podworny@monterail.com>
2016-04-29 10:08:28 +0200

    Add dependencies

--- mix.exs
+++ mix.exs
@@ -10,23 +10,16 @@ defmodule CoapNode.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [
+      applications: [:logger]
+    ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:gen_coap, git: "https://github.com/gotthardp/gen_coap.git"},
+      {:coap, git: "https://github.com/mskv/coap.git"}
+    ]
   end
 end
